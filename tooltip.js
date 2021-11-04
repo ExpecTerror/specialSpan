6d58b3157af602d50dfea37d62cd24db8e78e1e8
Petar D <duvnjakpetar990@gmail.com>
2021-11-04 21:52:14 +0100

    init

---
+++ tooltip.js
@@ -0,0 +1,62 @@
+
+// function surroundWithSpan(element){
+// const temp = element.getInnerHTML();
+// element.innerHTML = temp.replace("ETH","<span>ETH</span>")
+// }
+// ['ETH","BTC","ADA"]
+// getContent()
+// getParagraphs()
+// manipulatePArapgraphs()
+
+// main(){
+// const content - getContent();
+// const paraphs = getParagraphs(content);
+// manipulatePArapgraphs(paraphs);
+// }
+
+const singleContent = document.getElementsByClassName("single-content__wrap")
+
+singleContent
+
+const firstContent = singleContent[0] 
+
+firstContent
+
+firstContent.getInnerHTML()
+
+const x = firstContent.getElementsByTagName
+
+x
+
+const x = firstContent.getElementsByTagName("p")
+
+for(let i = 0; i > x.length; i++) {
+    console.log(i);
+    console.log(x[i].getInnerHTML());
+}
+
+
+const prviP = x[0];
+
+prviP
+
+prviP.innerHTML = "ETH";
+
+for(let i = 0; i < x.length; i++) {
+    console.log(i);
+    console.log(x[i].getInnerHTML());
+}
+
+for(let i = 0; i < x.length; i++) {
+    console.log(x.replace("ETH", "<span>ETH</span>"));
+}
+
+for(let i = 0; i < x.length; i++) {
+    const temp = x[i].getInnerHTML();
+ }
+
+
+for(let i = 0; i < x.length; i++) {
+    const temp = x[i].getInnerHTML();
+    x[i].innerHTML = temp.replace('ETH', '<span>ETH</span>');
+ }
